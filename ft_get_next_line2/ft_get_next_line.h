9cfa9bde2474e022419704640e6890b392ccd885
Pierre Bizien <pierre.bizien.1@gmail.com>
2022-11-08 15:46:26 +0100

    v0

---
+++ ft_get_next_line2/ft_get_next_line.h
@@ -0,0 +1,15 @@
+#ifndef FT_GET_NEXT_LINE_H
+#define FT_GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+
+# define BUFFER_SIZE 43
+
+# endif
+
+
+#endif
